3b98f20ec155132e47ab6f7899a64dd43e17f330
Erik Michaels-Ober <sferik@gmail.com>
2012-03-12 13:02:39 -0500

    Allow multiple file arguments

--- lib/travis/lint/runner.rb
+++ lib/travis/lint/runner.rb
@@ -16,45 +16,51 @@ module Travis
           end
         end
 
-        @travis_file_path = Pathname.new(argv.first).expand_path
+        @travis_yml_file_paths = []
+        argv.each do |arg|
+          @travis_yml_file_paths << Pathname.new(arg).expand_path
+        end
       end
 
 
       def run
-        check_that_travis_yml_file_exists!
-        check_that_travis_yml_file_is_valid_yaml!
+        errors = false
+        @travis_yml_file_paths.each do |travis_yml_file_path|
+          check_that_travis_yml_file_exists!(travis_yml_file_path)
+          check_that_travis_yml_file_is_valid_yaml!(travis_yml_file_path)
 
-        if (issues = Linter.validate(self.parsed_travis_yml)).empty?
-          puts "Hooray, .travis.yml at #{@travis_file_path} seems to be solid!"
-        else
-          issues.each do |i|
-            puts "Found an issue with the `#{i[:key]}:` key:\n\n\t#{i[:issue]}"
-            puts
-            puts
+          if (issues = Linter.validate(self.parsed_travis_yml(travis_yml_file_path))).empty?
+            puts "Hooray, #{travis_yml_file_path} seems to be solid!"
+          else
+            errors = true
+            puts "#{travis_yml_file_path} has issues:"
+            issues.each do |issue|
+              puts "  Found an issue with the `#{issue[:key]}:` key:\n    #{issue[:issue]}"
+            end
           end
-          quit ".travis.yml at #{@travis_file_path} has issues and thus will be ignored by Travis CI."
+          puts
         end
+        exit(1) if errors
       end
-
 
       protected
 
-      def check_that_travis_yml_file_exists!
-        quit("Cannot read #{@travis_file_path}: file does not exist or is not readable") unless File.exists?(@travis_file_path) &&
-          File.file?(@travis_file_path) &&
-          File.readable?(@travis_file_path)
+      def check_that_travis_yml_file_exists!(travis_yml_file_path)
+        quit("Cannot read #{travis_yml_file_path}: file does not exist or is not readable") unless File.exists?(travis_yml_file_path) &&
+          File.file?(travis_yml_file_path) &&
+          File.readable?(travis_yml_file_path)
       end
 
-      def check_that_travis_yml_file_is_valid_yaml!
+      def check_that_travis_yml_file_is_valid_yaml!(travis_yml_file_path)
         begin
-          YAML.load_file(@travis_file_path)
+          YAML.load_file(travis_yml_file_path)
         rescue ArgumentError, Psych::SyntaxError
-          quit ".travis.yml at #{@travis_file_path} is not a valid YAML file and thus will be ignored by Travis CI."
+          quit "#{travis_yml_file_path} is not a valid YAML file and thus will be ignored by Travis CI."
         end
       end
 
-      def parsed_travis_yml
-        YAML.load_file(@travis_file_path)
+      def parsed_travis_yml(travis_yml_file_path)
+        YAML.load_file(travis_yml_file_path)
       end
 
       def show_help
